0d3604b252632259e7093ba5f9f9e6ee7bf296ed
cor22982 <cor22982@uvg.edu.gt>
2024-07-13 00:12:55 -0600

    feat: Percentages

--- portafolio-mathew/src/PagesUP/ToolsUp/ToolsUp.jsx
+++ portafolio-mathew/src/PagesUP/ToolsUp/ToolsUp.jsx
@@ -6,13 +6,13 @@ const ToolsUp = () => {
   const toolsData = [
     { texto: "HTML", color: "#ffa260", hovercolor: "#e5ff60", percentage: 90, icon: "html" },
     { texto: "CSS", color: "#264de4", hovercolor: "#2965f1", percentage: 85, icon: "css" },
-    { texto: "JavaScript", color: "#f0db4f", hovercolor: "#efd81d", percentage: 80, icon: "javascript" },
+    { texto: "JavaScript", color: "#f0db4f", hovercolor: "#efd81d", percentage: 60, icon: "javascript" },
     { texto: "React", color: "#61dbfb", hovercolor: "#21a1f1", percentage: 75, icon: "react" },
     { texto: "Postgres", color: "#4faaff", hovercolor: "#64b2fa", percentage: 90, icon: "postgres" },
     { texto: "Github", color: "#bdbdbd", hovercolor: "#f3f3f3", percentage: 80, icon: "github" },
-    { texto: "ExpressJs", color: "#e1dcd7", hovercolor: "#fbff22", percentage: 70, icon: "express" },
+    { texto: "ExpressJs", color: "#e1dcd7", hovercolor: "#fbff22", percentage: 50, icon: "express" },
     { texto: "Aws", color: "#adb5c1", hovercolor: "#ff9900", percentage: 30, icon: "aws" },
-    { texto: "Nginx", color: "#64fa00", hovercolor: "#f4f4f2", percentage: 60, icon: "nginx" },
+    { texto: "Nginx", color: "#64fa00", hovercolor: "#f4f4f2", percentage: 30, icon: "nginx" },
     { texto: "Mysql", color: "#0992d6", hovercolor: "#f4ae6d", percentage: 60, icon: "mysql" },
     { texto: "Kotlin", color: "#f4ae6d", hovercolor: "#0992d6", percentage: 80, icon: "kotlin" }
 
